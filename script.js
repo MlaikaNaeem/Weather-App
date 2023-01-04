5f59d8fe2926126c4e7f4f50c332b7ec42b5383a
MlaikaNaeem <mlaikanaeem123@gmail.com>
2023-01-04 07:19:49 +0500

    Current weather API

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 function displayWeather(response) {
   console.log(response);
-  document.querySelector("#title").innerHTML = response.data.name;
+  document.querySelector("#title").innerHTML = response.data.city;
   document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
+    response.data.temperature.current
   );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#feels-like").innerHTML = Math.round(
-    response.data.main.feels_like
+    response.data.temperature.feels_like
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.condition.description;
 }
 
 function search(city) {
-  let apiKey = "bd5b4461863eddaa6ced0a0a67989e0a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "8e4abct0a56ee8b9ed8700oa801393f4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
 }
 function handleSubmit(event) {
