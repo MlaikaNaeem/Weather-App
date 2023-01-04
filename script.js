adb2bc9020a2408d04a968af381e31be8325e79b
MlaikaNaeem <mlaikanaeem123@gmail.com>
2023-01-04 09:04:27 +0500

    updated time

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
   console.log(response);
   document.querySelector("#title").innerHTML = response.data.city;
@@ -14,6 +38,9 @@ function displayWeather(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.condition.description;
+  document.querySelector("#time").innerHTML = formatDate(
+    response.data.time * 1000
+  );
 }
 
 function search(city) {
